7dd723a46de8351ac83672f79b549977747820e7
Shahjahan Mirza <103453568+ShahjahanMirza@users.noreply.github.com>
2023-05-29 02:05:31 +0500

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
+!pip install sklearn
+!pip install rake_nltk
+
 from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.feature_extraction.text import CountVectorizer
 
