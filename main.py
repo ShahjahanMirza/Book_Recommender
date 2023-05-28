de4f83059a1d6cce0672c8adb9c1816887fc6037
Shahjahan Mirza <103453568+ShahjahanMirza@users.noreply.github.com>
2023-05-29 02:11:52 +0500

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,6 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-pip install scikit-learn
 
 from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.feature_extraction.text import CountVectorizer
